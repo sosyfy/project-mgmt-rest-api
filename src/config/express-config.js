46a145937650545b9961c502d5654e9486ae5018
Sospeter Karanja <sospeterkaranja02@gmail.com>
2023-05-02 20:21:16 +0300

    initial setup

---
+++ src/config/express-config.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+import { app } from "../server.js";
+
+//middlewares
+import session from "../middlewares/session.js";
+import cors from "../middlewares/cors.js";
+import trimBody from "../middlewares/trimBody.js";
+
+export default function() {
+    
+	app.use(cors());
+	app.use(cookieParser());
+	app.use(bodyParser.json({
+		limit: "200kb"
+	}));
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(trimBody());
+	app.use(session());
+	app.use("/uploads", express.static("uploads"))//test here -> http://localhost:3000/uploads/rest-api.png
+}
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+//configurations
+import expressConfiguration from "./config/express-config.js";
+import databaseConfig from "./config/database-config.js";
+
+//routes
+import routes from "./routes/routes.js";
+import useDefaultRoute from "./routes/default-routes.js";
+
+
+export const app = express();
+
+expressConfiguration();
+databaseConfig()
+
+app.use(routes);
+useDefaultRoute();
+
+
